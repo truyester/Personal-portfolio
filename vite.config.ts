c4dfb625d07356dec65b7d386ea83a34cf206725
Truyester <william29854mendoza@gmail.com>
2026-04-18 11:03:46 -0400

    SEO: Add automatic sitemap generation

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    // 2. Configurar el Sitemap
+    Sitemap({ 
+      hostname: 'https://truyester.com',
+      dynamicRoutes: [
+        '/',
+        // Añade aquí otras rutas si llegas a tener /projects o /about por separado
+      ],
+      // Esto genera el archivo en la raíz de tu carpeta /dist
+    }),
+  ],
+});
